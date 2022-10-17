2bbd908bfcad2df4c431100d230b8888297a2985
Daym0sss <101497821+Daym0sss@users.noreply.github.com>
2022-10-18 00:03:43 +0300

    Create Task8Main.java

---
+++ task8/Task8Main.java
@@ -0,0 +1,8 @@
+package org.ivan.lab1.task8;
+
+public class Task8Main {
+    public static void main(String[] args) {
+        InsertionAlgorithm insertionAlgorithm = new InsertionAlgorithm();
+        insertionAlgorithm.printNewSequence();
+    }
+}
