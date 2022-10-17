7f261049a1aa32b1c8697880ef2001e31c45fc25
Daym0sss <101497821+Daym0sss@users.noreply.github.com>
2022-10-17 23:55:55 +0300

    Create Task4Main.java

---
+++ task4/Task4Main.java
@@ -0,0 +1,8 @@
+package org.ivan.lab1.task4;
+
+public class Task4Main {
+    public static void main(String[] args) {
+        PrimeNumberFinder primeNumberFinder = new PrimeNumberFinder();
+        primeNumberFinder.printPrimes();
+    }
+}
