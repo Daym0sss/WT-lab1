0ad5859118b13ef39d17b6e2a79a39621df233ec
Daym0sss <101497821+Daym0sss@users.noreply.github.com>
2022-10-17 23:45:32 +0300

    Create ExpressionResult.java

---
+++ ExpressionResult.java
@@ -0,0 +1,14 @@
+package org.ivan.lab1.task1;
+
+public class ExpressionResult {
+
+    public double getResult() {
+        double x = Math.random() * 100 - 50;
+        double y = Math.random() * 100 - 50;
+        return solveEquation(x, y);
+    }
+
+    private double solveEquation(double x, double y) {
+        return ((1 + Math.sin(x + y) * Math.sin(x + y)) / (2 + Math.abs(x - 2 * x / (1 + x * x * y * y)))) + x;
+    }
+}
