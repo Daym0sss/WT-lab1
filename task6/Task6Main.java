2fef206c06abcae7f92fc5245f8e9e0e824283cf
Daym0sss <101497821+Daym0sss@users.noreply.github.com>
2022-10-18 00:01:15 +0300

    Create Task6Main.java

---
+++ task6/Task6Main.java
@@ -0,0 +1,8 @@
+package org.ivan.lab1.task6;
+
+public class Task6Main {
+    public static void main(String[] args) {
+        MatrixInitializer matrixInitializer = new MatrixInitializer();
+        matrixInitializer.formMatrix();
+    }
+}
