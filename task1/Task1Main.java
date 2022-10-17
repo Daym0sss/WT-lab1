bdf8396d457de122f2a050bd1ad46c37169366da
Daym0sss <101497821+Daym0sss@users.noreply.github.com>
2022-10-17 23:51:02 +0300

    Create Task1Main.java

---
+++ task1/Task1Main.java
@@ -0,0 +1,9 @@
+package org.ivan.lab1.task1;
+
+public class Task1Main {
+    public static void main(String[] args) {
+        ExpressionResult expressionResult = new ExpressionResult();
+        double result = expressionResult.getResult();
+        System.out.println(result);
+    }
+}
