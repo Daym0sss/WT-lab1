2aaa925799dab00341962191cdf2d486c80ae62d
Daym0sss <101497821+Daym0sss@users.noreply.github.com>
2022-10-17 23:54:33 +0300

    Create Task3Main.java

---
+++ task3/Task3Main.java
@@ -0,0 +1,8 @@
+package org.ivan.lab1.task3;
+
+public class Task3Main {
+    public static void main(String[] args) {
+        FunctionExpression functionExpression = new FunctionExpression();
+        functionExpression.printResult();
+    }
+}
