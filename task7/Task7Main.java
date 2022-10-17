f1757c7e6d5b64b5556c9c3387b6b2394ed7c14a
Daym0sss <101497821+Daym0sss@users.noreply.github.com>
2022-10-18 00:02:15 +0300

    Create Task7Main.java

---
+++ task7/Task7Main.java
@@ -0,0 +1,8 @@
+package org.ivan.lab1.task7;
+
+public class Task7Main {
+    public static void main(String[] args) {
+        SortingAlgorithm sortingAlgorithm = new SortingAlgorithm();
+        sortingAlgorithm.sortArray();
+    }
+}
